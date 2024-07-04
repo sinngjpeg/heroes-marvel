30dc14fea0c95d25c11fb54c16ac767d14f5d790
Ingrid Silva <sinngtec@gmail.com>
2024-07-04 16:20:00 -0300

    change class name

--- app/src/main/java/br/com/sinngjpeg/marvelapp/presentation/favorites/FavoritesFragment.kt
+++ app/src/main/java/br/com/sinngjpeg/marvelapp/presentation/favorites/FavoritesFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.sinngjpeg.marvelapp.databinding.FragmentFavoriteBinding
 
-class FavoriteFragment : Fragment() {
+class FavoritesFragment : Fragment() {
     private lateinit var binding: FragmentFavoriteBinding
 
     override fun onCreateView(
