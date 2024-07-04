dd3374c4d25c981a5033d3b3c8770beda92048a3
Ingrid Silva <sinngtec@gmail.com>
2024-07-04 16:59:24 -0300

    add hilt to main activity

--- app/src/main/java/br/com/sinngjpeg/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/br/com/sinngjpeg/marvelapp/presentation/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import br.com.sinngjpeg.marvelapp.R
 import br.com.sinngjpeg.marvelapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
