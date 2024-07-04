cfc84e751b902a5c1bf7b8f087f923d47dedd5fb
Ingrid Silva <sinngtec@gmail.com>
2024-07-04 15:11:22 -0300

    add new module, git ignore

--- app/src/main/java/br/com/sinngjpeg/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/br/com/sinngjpeg/marvelapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.sinngjpeg.marvelapp
+package br.com.sinngjpeg.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.sinngjpeg.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
