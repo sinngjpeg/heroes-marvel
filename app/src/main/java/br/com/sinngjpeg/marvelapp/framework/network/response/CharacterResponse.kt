a34f317848b813d2151b0906d7136836df8d2873
Ingrid Silva <sinngtec@gmail.com>
2024-07-09 16:01:56 -0300

    add serializedname and description to characterresponse

--- app/src/main/java/br/com/sinngjpeg/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/br/com/sinngjpeg/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,16 +1,24 @@
 package br.com.sinngjpeg.marvelapp.framework.network.response
 
 import br.com.jpegsinng.core.domain.model.Character
+import com.google.gson.annotations.SerializedName
 
 data class CharacterResponse(
-    val id: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
     val name: String,
-    val thumbnail: ThumbnailResponse,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("thumbnail")
+    val thumbnail: ThumbnailResponse
 )
 
-fun CharacterResponse.toCharacter() : Character {
+fun CharacterResponse.toCharacterModel(): Character {
     return Character(
+        id = this.id,
         name = this.name,
-        thumbnail = "${this.thumbnail.path}.${this.thumbnail.extension}".replace("http", "https")
+        description,
+        imageUrl = this.thumbnail.getHttpsUrl()
     )
 }
