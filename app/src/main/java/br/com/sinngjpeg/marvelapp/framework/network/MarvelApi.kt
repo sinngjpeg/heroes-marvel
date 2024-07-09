d55bb2a617c0704dd8fd7c63a4883c1e6baf0da4
Ingrid Silva <sinngtec@gmail.com>
2024-07-09 15:50:13 -0300

    add new path to comics and events

--- app/src/main/java/br/com/sinngjpeg/marvelapp/framework/network/MarvelApi.kt
+++ app/src/main/java/br/com/sinngjpeg/marvelapp/framework/network/MarvelApi.kt
@@ -1,14 +1,32 @@
 package br.com.sinngjpeg.marvelapp.framework.network
 
+import br.com.sinngjpeg.marvelapp.framework.network.response.CharacterResponse
+import br.com.sinngjpeg.marvelapp.framework.network.response.ComicResponse
 import br.com.sinngjpeg.marvelapp.framework.network.response.DataWrapperResponse
+import br.com.sinngjpeg.marvelapp.framework.network.response.EventResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.QueryMap
 
 
 interface MarvelApi {
+
     @GET("characters")
     suspend fun getCharacters(
         @QueryMap
-        queries: Map<String, String>,
-    ): DataWrapperResponse
+        queries: Map<String, String>
+    ): DataWrapperResponse<CharacterResponse>
+
+    @GET("characters/{characterId}/comics")
+    suspend fun getComics(
+        @Path("characterId")
+        charactersId: Int
+    ): DataWrapperResponse<ComicResponse>
+
+    @GET("characters/{characterId}/events")
+    suspend fun getEvents(
+        @Path("characterId")
+        charactersId: Int
+    ): DataWrapperResponse<EventResponse>
+
 }
