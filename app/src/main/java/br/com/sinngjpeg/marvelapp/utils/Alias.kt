48e4978562054f87b35dbc64196e040551c93227
Ingrid Silva <sinngtec@gmail.com>
2024-07-09 13:28:40 -0300

    add alias to onclicked

---
+++ app/src/main/java/br/com/sinngjpeg/marvelapp/utils/Alias.kt
@@ -0,0 +1,6 @@
+package br.com.sinngjpeg.marvelapp.utils
+
+import android.view.View
+import br.com.jpegsinng.core.domain.model.Character
+
+typealias OnCharacterItemClick = (character: Character, view: View) -> Unit
