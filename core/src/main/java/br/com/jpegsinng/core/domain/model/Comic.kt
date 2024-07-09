9d9947882fa7a2d771210c2a327fcf935e9fc6f8
Ingrid Silva <sinngtec@gmail.com>
2024-07-09 13:50:49 -0300

    add new datas Comic and Event

---
+++ core/src/main/java/br/com/jpegsinng/core/domain/model/Comic.kt
@@ -0,0 +1,6 @@
+package br.com.jpegsinng.core.domain.model
+
+data class Comic(
+    val id: Int,
+    val imageUrl: String
+)
---
+++ core/src/main/java/br/com/jpegsinng/core/domain/model/Event.kt
@@ -0,0 +1,6 @@
+package br.com.jpegsinng.core.domain.model
+
+data class Event(
+    val id: Int,
+    val imageUrl: String
+)
