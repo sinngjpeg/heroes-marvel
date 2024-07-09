f781d3d6c75e682d9eefc6e8904e4648fd4cd9ed
Ingrid Silva <sinngtec@gmail.com>
2024-07-09 12:57:39 -0300

    add detail fragment

---
+++ app/src/main/java/br/com/sinngjpeg/marvelapp/presentation/detail/DetailFragment.kt
@@ -0,0 +1,30 @@
+package br.com.sinngjpeg.marvelapp.presentation.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.sinngjpeg.marvelapp.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+    private var _binding: FragmentDetailBinding? = null
+    private val binding: FragmentDetailBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ) = FragmentDetailBinding
+        .inflate(
+            inflater,
+            container,
+            false,
+        ).apply {
+            _binding = this
+        }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
