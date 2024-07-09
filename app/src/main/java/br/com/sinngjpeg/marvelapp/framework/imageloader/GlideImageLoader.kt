9da06029dad0993b9c88c60aca9a2708819f8a90
Ingrid Silva <sinngtec@gmail.com>
2024-07-09 13:19:54 -0300

    add glide interface

---
+++ app/src/main/java/br/com/sinngjpeg/marvelapp/framework/imageloader/GlideImageLoader.kt
@@ -0,0 +1,23 @@
+package br.com.sinngjpeg.marvelapp.framework.imageloader
+
+import android.widget.ImageView
+import br.com.sinngjpeg.marvelapp.R
+import com.bumptech.glide.Glide
+import javax.inject.Inject
+
+class GlideImageLoader @Inject constructor() : ImageLoader {
+
+    override fun load(
+        imageView: ImageView,
+        imageUrl: String,
+        placeholder: Int,
+        fallback: Int
+    ) {
+
+        Glide.with(imageView.rootView)
+            .load(imageUrl)
+            .placeholder(placeholder)
+            .fallback(R.drawable.ic_image_error)
+            .into(imageView)
+    }
+}
