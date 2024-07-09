1f644fc6cf3d08edcec87193bd129cd57e9c5ebc
Ingrid Silva <sinngtec@gmail.com>
2024-07-09 13:33:04 -0300

    add imageloader interface, onclicked implementation

--- app/src/main/java/br/com/sinngjpeg/marvelapp/presentation/home/CharactersViewHolder.kt
+++ app/src/main/java/br/com/sinngjpeg/marvelapp/presentation/home/CharactersViewHolder.kt
@@ -4,30 +4,39 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import br.com.jpegsinng.core.domain.model.Character
-import br.com.sinngjpeg.marvelapp.R
 import br.com.sinngjpeg.marvelapp.databinding.ItemCharacterBinding
-import com.bumptech.glide.Glide
+import br.com.sinngjpeg.marvelapp.framework.imageloader.ImageLoader
+import br.com.sinngjpeg.marvelapp.utils.OnCharacterItemClick
 
 class CharactersViewHolder(
     itemCharacterBinding: ItemCharacterBinding,
+    private val imageLoader: ImageLoader,
+    private val onItemClick: OnCharacterItemClick
 ) : RecyclerView.ViewHolder(itemCharacterBinding.root) {
-    private val imageCharacter = itemCharacterBinding.imageCharacter
+
     private val textName = itemCharacterBinding.textName
+    private val imageCharacter = itemCharacterBinding.imageCharacter
 
     fun bind(character: Character) {
         textName.text = character.name
-        Glide
-            .with(itemView)
-            .load(character.thumbnail)
-            .fallback(R.drawable.ic_image_error)
-            .into(imageCharacter)
+        imageCharacter.transitionName = character.name
+        imageLoader.load(imageCharacter, character.imageUrl)
+
+        itemView.setOnClickListener {
+            onItemClick.invoke(character, imageCharacter)
+        }
+
     }
 
     companion object{
-        fun create(parent: ViewGroup): CharactersViewHolder{
+        fun create(
+            parent: ViewGroup,
+            imageLoader: ImageLoader,
+            onItemClick: OnCharacterItemClick
+        ): CharactersViewHolder{
             val inflater  = LayoutInflater.from(parent.context)
             val itemCharacterBinding = ItemCharacterBinding.inflate(inflater, parent, false)
-            return CharactersViewHolder(itemCharacterBinding)
+            return CharactersViewHolder(itemCharacterBinding, imageLoader, onItemClick)
         }
     }
 
